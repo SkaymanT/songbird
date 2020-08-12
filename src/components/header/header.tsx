e3499281aec09c330cf33ced411705b64eb9f4d4
SkaymanT <SkaymanT@yandex.ru>
2020-08-12 13:42:57 +0300

    init: init react component

---
+++ src/components/header/header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import TopPanel from './top-panel';
+
+export default function Header(): JSX.Element {
+  return (
+    <div className="header">
+      <TopPanel />
+    </div>
+  );
+}
---
+++ src/components/header/top-panel.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function TopPanel(): JSX.Element {
+  return (
+    <div className="top-panel">
+      <div className="logo" />
+    </div>
+  );
+}
