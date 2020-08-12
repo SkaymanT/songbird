7ef398f6d66ad750e1f2bc395f14f6b1f50ae2d6
SkaymanT <SkaymanT@yandex.ru>
2020-08-12 13:47:29 +0300

    init: init header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-// import Header from './components/header/header';
+import Header from './components/header/header';
 
 
 function App(): JSX.Element {
   return (
     <div className="container">
       <BrowserRouter>
-        {/* <Header /> */}
+        <Header />
       </BrowserRouter>
     </div>
   );
